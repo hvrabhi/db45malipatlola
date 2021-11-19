d4da08ddb04311df39bf5092a6698590b8387d8c
hvrabhi <33493095+hvrabhi@users.noreply.github.com>
2021-11-18 21:10:00 -0600

    Updated app.js

--- app.js
+++ app.js
@@ -15,18 +15,33 @@ var marvelRouter = require('./routes/marvelcomics');
 
 
 var app = express();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  } ))
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString,
-{useNewUrlParser: true, useUnifiedTopology: true});
+  { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")
-recreateDB()
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+  recreateDB()
 });
 
 // view engine setup
@@ -37,6 +52,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -47,13 +69,22 @@ app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 app.use('/marvelcomics', marvelRouter);
 
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account')); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -66,23 +97,23 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 
-async function recreateDB(){
+async function recreateDB() {
   // Delete everything 
   await Costume.deleteMany();
 
-  let instance1 = new Costume({costume_type:"ghost", size:'large', cost:25.4});
-  let instance2 = new Costume({costume_type:"abhi", size:'large', cost:25.4});
-  let instance3 = new Costume({costume_type:"reddy", size:'large', cost:25.4});
-  instance1.save( function(err,doc) {
-  if(err) return console.error(err);
-  console.log("First object saved")
-  });
-  instance2.save( function(err,doc) {
-    if(err) return console.error(err);
+  let instance1 = new Costume({ costume_type: "ghost", size: 'large', cost: 25.4 });
+  let instance2 = new Costume({ costume_type: "abhi", size: 'large', cost: 25.4 });
+  let instance3 = new Costume({ costume_type: "reddy", size: 'large', cost: 25.4 });
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
     console.log("First object saved")
-    });
-  instance3.save( function(err,doc) {
-      if(err) return console.error(err);
-      console.log("First object saved")
-      });
-  }
+  });
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
+  });
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
+  });
+}
